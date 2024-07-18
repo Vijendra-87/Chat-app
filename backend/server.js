ed654b70d6b5fa3a42a5b38fed93815516a543a6
Vijendra-87 <119486760+Vijendra-87@users.noreply.github.com>
2024-07-18 11:12:26 +0530

    1st part completed  -backend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js"
+import messagesRoutes from "./routes/messages.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+dotenv.config(); 
+
+const app = express();
+const PORT = process.env.PORT || 5050
+
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messagesRoutes)
+app.use("/api/users",userRoutes)
+
+app.get("/",(req,res)=>{
+res.send("Hello World 1") 
+});
+
+
+app.listen(PORT,()=>{
+    connectToMongoDB()
+    console.log(`Server Running on Port ${PORT}`)
+    console.log(`http://localhost:${PORT}`)
+})
